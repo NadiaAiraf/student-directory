83fbb07bc02c55904aa9ab61546c4203eba6eb0d
Aidan Faria <aidanfaria@gmail.com>
2018-08-24 11:28:28 +0000

    add the load list case for students.csv

--- directory_by_cohort.rb
+++ directory_by_cohort.rb
@@ -74,6 +74,8 @@ def process(selection)
     print_footer
   when "3"
     save_students
+  when "4"
+    load_students
   when "9"
     exit
   else
@@ -85,6 +87,7 @@ def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save list of students to students.csv"
+  puts "4. Load list of students from students.csv"
   puts "9. Exit"
 end
 
@@ -94,6 +97,15 @@ def save_students
     student_data = [student[:name],student[:cohort]]
     csv_line = student_data.join(",")
     file.puts csv_line
+  end
+  file.close
+end
+
+def load_students
+  file = File.open("students.csv","r")
+  file.readlines.each do |line|
+    name, cohort = line.chomp.split(",")
+    @students << {name: name, cohort: cohort}
   end
   file.close
 end
